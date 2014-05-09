0dda17141ae8c4ffddbdc2a52c10304d9368716c
Paul Barford <Paul.Barford@paddypower.com>
2014-05-09 12:14:43 +0100

    spark-sandbox

--- spark-sandbox/src/main/scala/com/pjb/sandbox/NetworkWordCount.scala
+++ spark-sandbox/src/main/scala/com/pjb/sandbox/NetworkWordCount.scala
@@ -1,5 +1,4 @@
 package com.pjb.sandbox
-import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.StreamingContext._
 import org.apache.spark.storage.StorageLevel
@@ -9,11 +8,15 @@ object NetworkWordCount {
   def main(args: Array[String]) {
 
     StreamingExamples.setStreamingLogLevels()
-    val ssc = new StreamingContext(args(0), "NetworkWordCount", Seconds(10),
-      System.getenv("SPARK_HOME"), StreamingContext.jarOfClass(this.getClass).toSeq)
-    val lines = ssc.socketTextStream(args(1), args(2).toInt, StorageLevel.MEMORY_ONLY_SER)
+    val ssc = new StreamingContext("local[2]",
+                                   "NetworkWordCount",
+                                   Seconds(10),
+                                   System.getenv("SPARK_HOME"),
+                                   StreamingContext.jarOfClass(this.getClass).toSeq)
+    val lines = ssc.socketTextStream("localhost", 9999, StorageLevel.MEMORY_ONLY_SER)
+
     val words = lines.flatMap(_.split(" "))
-    val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
+    val wordCounts = words.map(x => (x, 1)).reduceByKey((a: Int, b: Int) => a + b)
     wordCounts.print()
     ssc.start()
     ssc.awaitTermination()
